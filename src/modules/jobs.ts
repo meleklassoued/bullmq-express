fa1f49f19994445914da68f40cc767d4e71a6433
meleklassoued <lassouedm07@gmail.com>
2025-07-01 16:19:33 +0100

    feat: Setup a single file as source of truth for all bullmq jobs

---
+++ src/modules/jobs.ts
@@ -0,0 +1,7 @@
+export { addWebhookJobs } from "./webhook/job";
+export { addReportJobs } from "./report/job";
+export { addEmailJobs } from "./email/job";
+export { addPaymentJobs } from "./payment/job";
+export { addOnboardingJobs } from "./onboarding/job";
+export { addImageProcessingJobs } from "./image/job";
+export { addBackupJobs } from "./backup/job";
