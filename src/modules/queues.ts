b9b66a8322d8dddbb0a7030573d559eac15af48e
meleklassoued <lassouedm07@gmail.com>
2025-07-01 15:39:25 +0100

    feat: Add queues and workers for processing various jobs

---
+++ src/modules/queues.ts
@@ -0,0 +1,7 @@
+export { emailQueue } from "../modules/email/queue";
+export { imageProcessingQueue } from "../modules/image/queue";
+export { paymentQueue } from "../modules/payment/queue";
+export { reportQueue } from "../modules/report/queue";
+export { webhookQueue } from "../modules/webhook/queue";
+export { userOnboardingQueue } from "../modules/onboarding/queue";
+export { dataBackupQueue } from "../modules/backup/queue";
---
+++ src/modules/workers.ts
@@ -0,0 +1,7 @@
+export { paymentWorker } from "./payment/worker";
+export { emailWorker } from "./email/worker";
+export { imageWorker } from "./image/worker";
+export { reportWorker } from "./report/worker";
+export { webhookWorker } from "./webhook/worker";
+export { onboardingWorker } from "./onboarding/worker";
+export { backupWorker } from "./backup/worker";
